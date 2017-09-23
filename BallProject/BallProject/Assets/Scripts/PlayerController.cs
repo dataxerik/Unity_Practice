9fd12914d425b18c8859a164846733757a495eb2
Xerik <dsshar@gmail.com>
2017-09-23 15:38:49 -0400

    added ball project tutorial

---
+++ BallProject/BallProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start() //First called method
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+	}
+
+
+	void Update() //Before rending the frame
+	{
+	}
+
+	void FixedUpdate() //For physics code
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 16) {
+			winText.text = "You Win!";
+		}
+	}
+}
+//Destroy (other.gameObject);
