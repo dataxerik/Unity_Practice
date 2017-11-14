5acc6a58a4f9921b1e1fd24e0bc8b0746a82a1a8
Xerik <dsshar@gmail.com>
2017-11-14 11:20:52 -0500

    added project

---
+++ CollisionDetection/Assets/Script/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 6;
+
+	float coinCount;
+	Rigidbody myRigidbody;
+	Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 direction = input.normalized;
+		velocity = direction * speed;
+		print ("Coin count of " + coinCount);
+	}
+
+	void FixedUpdate(){
+		myRigidbody.position += velocity * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider triggerCollider) {
+		if(triggerCollider.gameObject.tag.Equals("Coin")){
+			Destroy(triggerCollider.gameObject);
+			coinCount++;
+		}
+	}
+}
---
+++ Rotations/Assets/Script/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float speed = 10f;
+
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		float z = Input.GetAxisRaw ("Vertical");
+		transform.Translate (new Vector3(x, 0, z) * speed * Time.deltaTime);
+	}
+}
